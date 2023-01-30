a7a3ed73eed179acda724bb713bec5078cddc753
ShivamKumarJha <jha.shivam3@gmail.com>
2023-01-30 00:50:15 +0000

    Add drivers/media/platform/msm/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_ois/cam_ois_thread.h
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_ois/cam_ois_thread.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -9,13 +9,14 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+#ifndef _CAM_OIS_THREAD_H_
+#define _CAM_OIS_THREAD_H_
 
-#ifndef _CAM_IR_LED_SOC_H_
-#define _CAM_IR_LED_SOC_H_
+#include "cam_ois_dev.h"
 
-#include "cam_ir_led_dev.h"
+int cam_ois_thread_add_msg(struct cam_ois_ctrl_t *o_ctrl, struct cam_ois_thread_msg_t *msg);
+int cam_ois_thread_create(struct cam_ois_ctrl_t *o_ctrl);
+int cam_ois_thread_destroy(struct cam_ois_ctrl_t *o_ctrl);
 
-int cam_ir_led_get_dt_data(struct cam_ir_led_ctrl *fctrl,
-	struct cam_hw_soc_info *soc_info);
-
-#endif /*_CAM_IR_LED_SOC_H_*/
+#endif
+/* _CAM_OIS_THREAD_H_ */
